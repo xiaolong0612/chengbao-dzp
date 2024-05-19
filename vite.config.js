9d9dbc55289772e956dd8304771b5701e3f6ebd9
xiaolong0612 <1057520381@qq.com>
2024-05-20 00:45:25 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		assetsDir: 'index'
-	},
   plugins: [
     vue(),
     Components({
